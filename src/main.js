c534dedc0ccf61e53218b256c9fa7ba069734e88
Alex <alex@frisch.me>
2023-02-12 14:27:57 +0100

    Initial commit

--- src/main.js
+++ src/main.js
@@ -4,9 +4,9 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import { router } from './router';
 
-// setup fake backend
-import { fakeBackend } from './helpers';
-fakeBackend();
+//setup fake backend
+//import { fakeBackend } from './helpers';
+//fakeBackend();
 
 const app = createApp(App);
 
