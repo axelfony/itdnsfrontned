f3c04f643051112bec44e2104c5a744f36db2b5b
Alex <alex@frisch.me>
2023-02-15 15:23:58 +0100

    Navbar workin with tailwind

--- src/views/account/Layout.vue
+++ src/views/account/Layout.vue
@@ -9,7 +9,8 @@ if (authStore.user) {
 }
 </script>
 
-<template>
+<!-- Bootstrap -->
+<!-- <template>
     <div class="container">
         <div class="row">
             <div class="col-sm-8 offset-sm-2 mt-5">
@@ -17,4 +18,15 @@ if (authStore.user) {
             </div>
         </div>
     </div>
-</template>
+</template> -->
+<!-- Tailwind -->
+<template>
+    <div class="container mx-auto p-4">
+      <div class="flex justify-center">
+        <div class="w-full md:w-2/3 lg:w-1/2 mt-5">
+          <router-view />
+        </div>
+      </div>
+    </div>
+ </template>
+  
